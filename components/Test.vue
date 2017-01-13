fcd7b60d63dca417cc8fd588d8e1bb3e7b1a718e
Daniel Prado <danielprado.ad@gmail.com>
2017-01-13 11:13:02 -0200

    initial

---
+++ components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Test Component
+    <br />
+    <p>
+      <strong>From api: </strong>
+      {{title}}
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return axios.get('http://jsonplaceholder.typicode.com/photos/4').then(res => res.data);
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    Home 2
+    <p>
+      <strong>From api: </strong>
+      {{title}}
+    </p>
+
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <test-component />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import TestComponent from '../components/Test.vue';
+
+export default {
+  components: { TestComponent },
+  data() {
+    return axios.get('http://jsonplaceholder.typicode.com/photos/4').then(res => res.data);
+  }
+}
+</script>
+
+<style>
+.container {
+  text-align: center;
+  padding-top: 100px;
+  font-family: sans-serif;
+}
+</style>
